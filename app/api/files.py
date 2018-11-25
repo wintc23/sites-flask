a7f97c66f5f6a87cf24699c36c529981ffa11b94
Lushenggang <lushg-tcxg@qq.com>
2018-11-25 20:26:42 +0800

    files

--- app/api/files.py
+++ app/api/files.py
@@ -13,13 +13,13 @@ def save_image():
   f = request.files['image']
   filename = str(uuid.uuid1()) + f.filename
   dirname, _ = os.path.split(os.path.abspath(sys.argv[0]))
-  upload_path = dirname + '/files/'
+  upload_path = dirname + '../files/'
   f.save(upload_path + filename)
   return jsonify({ 'message': '上传成功', 'filename': filename })
 
 @api.route('/get-file/', methods = ['GET'])
 def get_file():
   dirname, _ = os.path.split(os.path.abspath(sys.argv[0]))
-  dirpath = dirname + '/files/'
+  dirpath = dirname + '../files/'
   filename = request.args.get('filename')
   return send_from_directory(dirpath, filename)
